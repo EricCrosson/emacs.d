1d4a54e7f2f155ef61c5404dfb14a69f2151448c
Eric Crosson <eric.s.crosson@utexas.com>
2018-12-17 10:07:46 -0600

    Add script to download hack-font

---
+++ bin/font-install-hack.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Written by Eric Crosson
+# 2018-12-15
+#
+# Install Hack-font (currently only supports Mac OS)
+#
+# Takes no arguments.
+
+brew cask install homebrew/cask-fonts/font-hack
